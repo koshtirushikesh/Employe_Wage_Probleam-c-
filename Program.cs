da700f7453dafd7b6f80fe7c2f16ac52dbbd5a68
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-14 09:18:30 +0530

    Greating massage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageProblem
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employe Wage Probleam");
+
+            EmployeCheck.CheckAttendence();
+        }
+    }
+}
