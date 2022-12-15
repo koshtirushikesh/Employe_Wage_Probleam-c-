1fafbdc26234615f5fd56b03d194ad3da89c7234
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-15 10:09:56 +0530

    Refractor: with caase for part time employee

--- EmployeCheck.cs
+++ EmployeCheck.cs
@@ -8,33 +8,30 @@ namespace EmployeeWageProblem
 {
     public class EmployeCheck
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public static void CheckAttendence()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            
             int empHrs = 0;
-            int EMP_RATE_PER_HOUR = 20;
             int empWage = 0;
+            int EMP_RATE_PER_HOUR = 20;
 
             Random random = new Random();
             int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME )
-            {
-                empHrs = 4;
-            }
-            else
-            {             
-                empHrs = 0;
+                case IS_FULL_TIME: empHrs = 8; break;
+                case IS_PART_TIME: empHrs = 4; break;
+
+                default: empHrs = 0; break;
+
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employe Wage: " + empWage);
+            Console.WriteLine(": " + empWage);
         }
-
     }
 }
