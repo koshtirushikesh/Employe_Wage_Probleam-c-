56dff220f3a2af4f42bfd8d1c2a6c36439e9f92a
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-15 09:20:08 +0530

    added the prt time

--- EmployeCheck.cs
+++ EmployeCheck.cs
@@ -11,16 +11,29 @@ namespace EmployeeWageProblem
         public static void CheckAttendence()
         {
             int IS_FULL_TIME = 1;
-            
-            Random random = new Random();
-            int empCheck = random.Next(2);
+            int IS_PART_TIME = 2;
+            int empHrs = 0;
+            int EMP_RATE_PER_HOUR = 20;
+            int empWage = 0;
 
-            if (empCheck == IS_FULL_TIME) {
-                Console.WriteLine("Employee is Present");
-            }else
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 8;
             }
+            else if (empCheck == IS_PART_TIME )
+            {
+                empHrs = 4;
+            }
+            else
+            {             
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employe Wage: " + empWage);
         }
 
     }
