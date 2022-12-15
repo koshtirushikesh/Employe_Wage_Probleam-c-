f29aa9428a3524ba86d9237a0fa8169191eb0cb2
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-15 10:51:59 +0530

    Wages for a Month

--- EmployeCheck.cs
+++ EmployeCheck.cs
@@ -8,19 +8,37 @@ namespace EmployeeWageProblem
 {
     public class EmployeCheck
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
         public static void CheckAttendence()
         {
-            int IS_FULL_TIME = 1;
-            
-            Random random = new Random();
-            int empCheck = random.Next(2);
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
 
-            if (empCheck == IS_FULL_TIME) {
-                Console.WriteLine("Employee is Present");
-            }else
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                Console.WriteLine("Employee is Absent");
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME: empHrs = 8; break;
+                    case IS_PART_TIME: empHrs = 4; break;
+
+                    default: empHrs = 0; break;
+
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine(": " + empWage);
+                totalEmpWage += empWage;
             }
+            Console.WriteLine("total employee wage:" + totalEmpWage);
+
         }
 
     }
